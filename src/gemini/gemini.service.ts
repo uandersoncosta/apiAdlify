433c547a84ee4bc13fdc0a4107145e509874e7fb
Uanderson Costa <uandersonferreiracosta@gmail.com>
2025-09-03 22:16:04 -0300

    feat: adicionando a função generateImage no service

--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { GoogleGenAI } from '@google/genai';
 
 @Injectable()
-export class GeminiService {}
+export class GeminiService {
+  private ai: GoogleGenAI;
+
+  constructor() {
+    this.ai = new GoogleGenAI({
+      apiKey: process.env.GOOGLE_API_KEY,
+    });
+  }
+
+  async generateImage(prompt: string): Promise<Buffer> {
+    const response = await this.ai.models.generateContent({
+      model: 'gemini-2.5-flash-image-preview',
+      contents: prompt,
+    });
+
+    if (!response.candidates || response.candidates.length === 0) {
+      throw new Error('Nenhuma resposta de candidato retornada pelo Gemini.');
+    }
+
+    const parts = response.candidates[0].content?.parts;
+    const part = parts?.find((p: any) => p.inlineData);
+
+    if (!part?.inlineData?.data) {
+      throw new Error('Nenhuma imagem encontrada na resposta do Gemini.');
+    }
+
+    const imageData: string = part.inlineData.data;
+    return Buffer.from(imageData, 'base64');
+  }
+}
