9eec4d0be770fa5ebd54312750d067e91a2b1d92
Uanderson Costa <uandersonferreiracosta@gmail.com>
2025-09-03 22:23:28 -0300

    chore: adicionando o swagger na main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Adlify')
+    .setDescription('The api that you can generate the best image to your post to your instagram/linkedin/facebook')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
