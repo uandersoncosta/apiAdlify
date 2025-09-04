575a57bf252984e92dad6c93ef1015ff7afa5258
Uanderson Costa <uandersonferreiracosta@gmail.com>
2025-09-03 22:20:36 -0300

    feat: adicionando o metodo getImage(POST) no controller

--- src/gemini/gemini.controller.ts
+++ src/gemini/gemini.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Res } from '@nestjs/common';
+import type { Response } from 'express';
 import { GeminiService } from './gemini.service';
+import { PromptDto } from './Dtos/PromptDto';
 
 @Controller('gemini')
 export class GeminiController {
   constructor(private readonly geminiService: GeminiService) {}
+
+  @Post('image')
+  async getImage(@Body() body: PromptDto, @Res() res: Response) {
+    try {
+      const buffer = await this.geminiService.generateImage(body.prompt);
+
+      res.setHeader('Content-Type', 'image/png');
+      res.send(buffer);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
 }
